4c6749d4e2a7f00d609c59251b186a00e169ae1d
Madhav Madhusoodanan <madhavmadhusoodanan@gmail.com>
2026-07-19 23:24:19 +0530

    Create scenario_drag_deorbit.rs

--- examples/scenario_basic_orbit.rs
+++ examples/scenario_basic_orbit.rs
@@ -116,7 +116,7 @@ fn main() {
         initial_velocity_mps: v0,
         initial_sigma_bn: Vector3::zeros(),
         initial_omega_radps: Vector3::zeros(),
-        integrator: None
+        integrator: None,
     });
 
     if use_sh {
